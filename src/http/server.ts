05e1c2aa773a2d251b950f9fb1fa507a2f93cbd1
williamasjr <wasjr00@gmail.com>
2024-02-06 18:29:33 -0300

    feat: add vote model and endpoint to crete a new vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,10 +4,18 @@ import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "polls-app-secret",
+  hook: "onRequest",
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => console.log("HTTP server running!"));
