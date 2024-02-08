00df7b5c69bbc2a461bc30a8ea6de63c60966246
williamasjr <wasjr00@gmail.com>
2024-02-08 15:08:30 -0300

    feat: implements web socket server side communication

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,12 +1,21 @@
+import z from "zod";
+
 import { FastifyInstance } from "fastify";
+import { voting } from "../../utils/votin-pub-sub";
 
 export async function pollResults(app: FastifyInstance) {
   app.get(
     "/polls/:pollId/results",
     { websocket: true },
     (connection, request) => {
-      connection.socket.on("message", (message: string) => {
-        connection.socket.send("you sent: " + message);
+      const getPollParams = z.object({
+        pollId: z.string().uuid(),
+      });
+
+      const { pollId } = getPollParams.parse(request.params);
+
+      voting.subscriber(pollId, (message) => {
+        connection.socket.send(JSON.stringify(message));
       });
     }
   );
